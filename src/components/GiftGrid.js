e9392b6ff2492dba7d380adc082b4f416005d5c8
Pablo Roman <pablorcb117@gmail.com>
2021-12-29 18:47:55 -0600

    Primer commit

---
+++ src/components/GiftGrid.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react'
+
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GiftGrid = ({category}) => {
+
+    const {data,loading} = useFetchGifs(category);
+    
+
+    return (
+        <> 
+        
+        <h3 className="animate__animated animate__fadeIn">{category}</h3>
+        {loading && <p className="animate__animated animate__flash">Cargando...</p> }
+        <div className='card-grid'>
+            
+            
+               {
+                   data.map(img => (
+                       <GifGridItem
+                       key={img.id}
+                       {...img } />
+                   ))
+               }
+            
+        </div>
+        
+        </>
+
+    )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(category)}&api_key=OqPMy6vFb07lKbhJwQYi8Kdgmmk5k592`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+        return gifs;
+       
+
+}
